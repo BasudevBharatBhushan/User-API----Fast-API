5a35d757ef8587c66b56f8e9409f6fe2f19a6cd0
BasudevBharatBhushan <bharatbhushan173@gmail.com>
2023-03-21 22:37:38 +0530

    update user.py

--- user.py
+++ user.py
@@ -15,41 +15,16 @@ class User(BaseModel):
 
 def __init__(
         self, 
-        chad_id: str,
+
         wallet_id: str,
-        discord_id: str,
-        twitter_id: str,
-        c3: int,
-        created_at: datetime = datetime.now(),
-        updated_at: datetime = datetime.now(),
+    
         ) -> None:
-        self.chad_id = chad_id
-        if not isinstance(self.chad_id, str):
-            raise TypeError("chad id should be a string")
-        
+
         self.wallet_id = wallet_id
         if not isinstance(self.wallet_id, str):
             raise TypeError("wallet id should be a string")
         
-        self.discord_id = discord_id
-        if not isinstance(self.discord_id, str):
-            raise TypeError("discord id should be a string")
-        
-        self.twitter_id = twitter_id
-        if not isinstance(self.twitter_id, str):
-            raise TypeError("twitter id should be a string")
-        
-        self.c3 = c3
-        if not isinstance(self.c3, int):
-            raise TypeError("c3 should be a int")
-        
-        self.created_at = created_at
-        if not isinstance(self.created_at, datetime):
-            raise TypeError("created at should be a datetime")
-        
-        self.updated_at = updated_at
-        if not isinstance(self.updated_at, datetime):
-            raise TypeError("updated at should be a datetime")
+
         
 
 def to_dict(self):
